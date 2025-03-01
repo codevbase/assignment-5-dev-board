6c3290428d38a6cbb85e2d8cc276ece18ebca183
lab-machine <ahasanhabib51971@gmail.com>
2025-03-02 05:26:52 +0600

    handled all buttons

--- scripts/task.js
+++ scripts/task.js
@@ -1,19 +1,45 @@
-document.getElementById("shop-ease").addEventListener("click", function() {
-    const taskElement = document.getElementById("task-assign").innerText;
-    const taskFinishElement = document.getElementById("task-finish").innerText;
+// document.getElementById("shop-ease").addEventListener("click", function() {
+//     const taskElement = document.getElementById("task-assign").innerText;
+//     const taskFinishElement = document.getElementById("task-finish").innerText;
     
-    let task = parseInt(taskElement);
-    task = task - 1;
-    document.getElementById("task-assign").innerText = task;
+//     let task = parseInt(taskElement);
+//     task = task - 1;
+//     document.getElementById("task-assign").innerText = task;
 
-    let taskFinish = parseInt(taskFinishElement);
-    taskFinish = taskFinish + 1;
-    document.getElementById("task-finish").innerText = taskFinish;
+//     let taskFinish = parseInt(taskFinishElement);
+//     taskFinish = taskFinish + 1;
+//     document.getElementById("task-finish").innerText = taskFinish;
 
 
-    document.getElementById("shop-ease").disabled = true;
-    document.getElementById("shop-ease").style.backgroundColor = "gray-100";
-});
+//     document.getElementById("shop-ease").disabled = true;
+//     document.getElementById("shop-ease").style.backgroundColor = "gray-100";
+// });
+const buttons  = document.getElementsByClassName("ok-btn");
+for(const button of buttons) {
+    button.addEventListener("click", function() {
+        const taskElement = document.getElementById("task-assign").innerText;
+        const taskFinishElement = document.getElementById("task-finish").innerText;
+        
+        let task = parseInt(taskElement);
+        task = task - 1;
+        document.getElementById("task-assign").innerText = task;
+
+        let taskFinish = parseInt(taskFinishElement);
+        taskFinish = taskFinish + 1;
+        document.getElementById("task-finish").innerText = taskFinish;
+
+    
+       
+        button.disabled = true;
+        button.style.backgroundColor = "gray-100";
+
+        if(task === 0) {
+            alert("All tasks are done!");
+        }
+
+    });
+}
+
 
 function updateDate() {
     const date = new Date();
